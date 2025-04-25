870fbfad47672ac227354fec0f059eac99f7ef08
ExxaEcosystem <y154y154@gmail.com>
2025-04-25 21:25:09 +0400

    Update ExxaBaseStorage.sol

--- contracts/ExxaBaseStorage.sol
+++ contracts/ExxaBaseStorage.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.29;
-
+//
 abstract contract ExxaBaseStorage {
     // Counter to assign a unique ID to each user investment
     uint256 internal globalInvestmentId;
